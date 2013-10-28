a943a73723dd7686a4cae5de676753cda365f820
tokuhirom <tokuhirom@gmail.com>
2013-10-28 15:59:05 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::NonCore - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::NonCore;
+
+# DESCRIPTION
+
+Amon2::NonCore is ...
+
+# LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokuhirom <tokuhirom@gmail.com>
---
+++ lib/Amon2/NonCore.pm
@@ -0,0 +1,39 @@
+package Amon2::NonCore;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::NonCore - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::NonCore;
+
+=head1 DESCRIPTION
+
+Amon2::NonCore is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
